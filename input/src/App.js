7b79895126f36f11ada5512e30f384c24e994a03
Farzad YZ <farskid@gmail.com>
2019-11-09 00:13:44 +0300

    debouncing and cancellation

--- input/src/App.js
+++ input/src/App.js
@@ -3,13 +3,29 @@ import logo from "./logo.svg";
 import "./App.css";
 import {
   Machine,
-  assign
+  assign,
+  actions
 } from "xstate";
 import { useMachine } from "@xstate/react";
 
 function isEmailvalid(email) {
   return /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.com/.test(
     email
+  );
+}
+
+function checkValidityService(email) {
+  return new Promise(
+    (resolve, reject) => {
+      console.log(
+        "check validity fired"
+      );
+      setTimeout(() => {
+        Math.random() > 0.5
+          ? resolve(email)
+          : reject(email);
+      }, 5000);
+    }
   );
 }
 
@@ -46,7 +62,38 @@ const inputStates = {
       },
       states: {
         empty: {},
-        valid: {},
+        valid: {
+          initial: "waiting",
+          states: {
+            waiting: {
+              // Deboucing!
+              after: {
+                1000: "pending"
+              }
+            },
+            pending: {
+              entry:
+                "initializeController",
+              invoke: {
+                src: "checkValid",
+                onDone: "available",
+                onError: "notAvailable"
+              },
+              exit: [
+                "cancelRequest",
+                "resetController"
+              ]
+            },
+            available: {
+              entry: (_, e) =>
+                console.log(e.data)
+            },
+            notAvailable: {
+              entry: (_, e) =>
+                console.log(e.data)
+            }
+          }
+        },
         invalid: {}
       }
     }
@@ -58,7 +105,26 @@ const statechart = Machine(inputStates);
 function App() {
   const [state, sendEvent] = useMachine(
     statechart.withConfig({
+      services: {
+        checkValid: ctx =>
+          checkValidityService(
+            ctx.value,
+            ctx.reqCtrl.signal
+          )
+      },
       actions: {
+        initializeController: assign({
+          reqCtrl: new AbortController()
+        }),
+        resetController: assign({
+          reqCtrl: undefined
+        }),
+        cancelRequest: ctx => {
+          console.log(
+            "Aborting validity request due to input change!"
+          );
+          // ctx.reqCtrl.abort();
+        },
         saveValue: assign({
           value: (_, e) => e.data
         })
@@ -104,8 +170,31 @@ function App() {
         {state.matches({
           ready: "invalid"
         }) && (
-          <p className="error">
+          <p className="warning">
             The email is invalid
+          </p>
+        )}
+        {state.matches({
+          ready: {
+            valid: "pending"
+          }
+        }) && <p>Checking...</p>}
+        {state.matches({
+          ready: {
+            valid: "available"
+          }
+        }) && (
+          <p className="success">
+            Email is available!
+          </p>
+        )}
+        {state.matches({
+          ready: {
+            valid: "notAvailable"
+          }
+        }) && (
+          <p className="error">
+            Email is taken!
           </p>
         )}
       </header>
